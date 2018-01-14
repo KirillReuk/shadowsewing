e92bfa33cd01e3fc18105cca306b97ec27ec520a
Kirill Reuk <kirillreuk@gmail.com>
2018-01-15 01:17:11 +0500

    heavy optimization plus some bugs fixed

--- main.py
+++ main.py
@@ -9,10 +9,10 @@ def dopixelsneighbour(pixel1, pixel2):
     diffy = abs(pixel1[1] - pixel2[1])
     return (diffx < 2) & (diffy < 2)
 
-def distancefromsun(pixel, angle): #
+def distancefromsun(pixel, angle):
     return pixel[0] * math.cos(angle) - pixel[1] * math.sin(angle)
 
-def distancebetweenpixels(pixel1, pixel2): #
+def distancebetweenpixels(pixel1, pixel2):
     return math.sqrt((pixel1[0] - pixel2[0]) * (pixel1[0] - pixel2[0]) + (pixel1[1] - pixel2[1]) * (pixel1[1] - pixel2[1]))
 
 def reversecolor(color):
@@ -34,10 +34,8 @@ imagescale = 1
 
 houseImage = cv2.imread("bighouses.png",0)
 imgheight, imgwidth = houseImage.shape
+imgsize = imgheight * imgwidth
 houseImage=cv2.copyMakeBorder(houseImage, top=1, bottom=1, left=1, right=1, borderType= cv2.BORDER_CONSTANT, value=255 ) #добавляем границу в 1 пиксель
-#cv2.imshow('image',houseImage)
-#cv2.waitKey(0)
-#cv2.destroyAllWindows()
 
 shadowImage = cv2.imread("bigshadows.png",0)
 kernel = np.ones((5,5),np.uint8)
@@ -57,30 +55,28 @@ count = 0
 #1. проходим по всем пикселям и собираем их в дома (связные области черного цвета)
 
 queue = []
-usedpixels = []
+usedpixels = np.zeros((imgwidth + 2, imgheight + 2))
 
 
-for x in range(1, imgwidth - 1):
-    for y in range(1, imgheight - 1):
+for x in range(1, imgwidth + 1):
+    for y in range(1, imgheight + 1):
 
         count += 1
-        print("total", count, "of", imgheight*imgwidth, "(", round(100 * count / imgheight / imgwidth, 3), "%)")
+        print("total", count, "of", imgsize, "(", round(100 * count / imgsize, 3), "%)")
 
         if houseImage[y, x] == 0:
-            if (x, y) not in usedpixels:
-                usedpixels.append((x, y))
+            if usedpixels[x, y] == 0:
+                usedpixels[x, y] = 1  # найден новый дом
                 queue.append((x, y))
                 house = []
 
-                while len(queue) > 0:
-                    #print("queue length:", len(queue), "top pixel:", queue[0])
+                while len(queue) > 0: #через flood fill собираем этот дом по пикселям
+                    print("queue length:", len(queue), "top pixel:", queue[0])
                     cur = queue.pop(0)
                     house.append((cur[0] - 1, cur[1] - 1)) #вычитаем 1 из-за добавленной границы
                     for xshift, yshift in [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]:
-                        #if (0 <= (cur[1] + yshift) < imgheight) & (0 <= (cur[0] + xshift) < imgwidth) & ((cur[0] + xshift, cur[1] + yshift) not in usedpixels):
-                        #    if (houseImage[cur[1] + yshift, cur[0] + xshift] == 0):
-                        if ((cur[0] + xshift, cur[1] + yshift) not in usedpixels) & (houseImage[cur[1] + yshift, cur[0] + xshift] == 0):
-                                usedpixels.append((cur[0] + xshift, cur[1] + yshift))
+                        if (usedpixels[cur[0] + xshift, cur[1] + yshift] == 0) & (houseImage[cur[1] + yshift, cur[0] + xshift] == 0):
+                                usedpixels[cur[0] + xshift, cur[1] + yshift] = 1
                                 queue.append((cur[0] + xshift, cur[1] + yshift))
 
                 houses.append(house)
@@ -94,8 +90,8 @@ print("step 2: house sorting complete")
 
 #3. создаем лист пикселей теней для перебора
 shadowpixels = []
-for x in range(imgheight):
-    for y in range(imgwidth):
+for x in range(imgwidth):
+    for y in range(imgheight):
         # pixel = shadowImage.getpixel((x, y))
         pixel = shadowImage[y][x]
         if pixel == 0:
@@ -145,14 +141,13 @@ for ind, house in enumerate(houses):
     result[tuple(house[0])] = [house, pivotshadow] #словарь: ключи - верхние левые пиксели, значения - пары: пиксели дома, пиксели тени
     print("shadow", ind, "/", len(houses), "cleanup complete")
 
+#рисуем результат
 d = ImageDraw.Draw(emptyCanvas)
-count = 0
-step = 255 // len(result)
 for key in result:
     for s in result[key][1]:
-        emptyCanvas.putpixel(s, (255 - step * count, step * count, 255 - step * count))
+        emptyCanvas.putpixel(s, (255, 0, 255))
     for h in result[key][0]:
-        emptyCanvas.putpixel(h, (step * count, 255 - step * count, step * count))
+        emptyCanvas.putpixel(h, (0, 255, 0))
 
     farthestHousePix = max(result[key][0], key=lambda x: distancefromsun(x, illuminationangle))
     if len(result[key][1]) != 0:#у здания есть тень
@@ -163,7 +158,6 @@ for key in result:
         height = 0
 
     d.text(key, str(round(height, 2)))
-    count += 1
 print(len(houses))
 
 emptyCanvas.show()
